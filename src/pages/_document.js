83dd94a917c0674edc23d6830f75299f203e6b9d
jorgecontrerasostos <jorge.contreras@justia.com>
2024-02-07 14:52:30 -0600

    add ga tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,14 @@
-import { GoogleAnalytics } from '@next/third-parties/google'
 import { Head, Html, Main, NextScript } from 'next/document'
+import GoogleAnalytics from '../components/GoogleAnalytics'
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head />
       <body>
-        <GoogleAnalytics gaId='G-CRDC850GFQ' />
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+          <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+        ) : null}
         <Main />
         <NextScript />
       </body>
