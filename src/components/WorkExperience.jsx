e856b8d80e1e84c3b480fe6cbb099424ca43462f
jorgecontrerasostos <jorge.contreras@justia.com>
2024-02-06 19:04:36 -0600

    add responsivity

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -1,45 +1,54 @@
 import { Link } from '@chakra-ui/next-js'
-import { Box, Code, Flex, Heading, Spacer, Text } from '@chakra-ui/react'
+import { Box, Code, Flex, Heading, Spacer, Stack, Text } from '@chakra-ui/react'
 import { jobs } from '../data/jobs'
 const WorkExperience = () => {
   return (
-    <Box my={8}>
+    <Stack my={8}>
       <Box>
         <Heading
           as='h2'
           fontSize='1.5em'
-          textAlign={['center', 'center', 'center', 'left', 'left', 'left']}
+          textAlign={['center', 'center', 'left', 'left', 'left', 'left']}
           letterSpacing='tight'
-          mb={4}
+          mb={2}
         >
           Work Experience
         </Heading>
       </Box>
       {jobs.map((job) => {
         return (
-          <Box key={job.id} py={2}>
-            <Flex alignItems='center' gap={2}>
-              <Heading as='h2' fontSize='1.2em'>
-                {job.title} -
-              </Heading>
-              <Link href={job.link} target='_blank'>
-                <Code
-                  colorScheme={job.company === 'Justia' ? 'blue' : 'red'}
-                  borderRadius='md'
-                  px={2}
-                >
-                  {job.company}
-                </Code>
-              </Link>
+          <Stack key={job.id} py={2} direction='column'>
+            <Flex
+              alignItems='flex-start'
+              gap={2}
+              direction={['column', 'column', 'row', 'row', 'row', 'row']}
+              px={[2, 2, 0]}
+            >
+              <Flex gap={4}>
+                <Heading as='h2' fontSize='1.2em'>
+                  {job.title}
+                </Heading>
+                <Link href={job.link} target='_blank' alignItems='flex-end'>
+                  <Code
+                    colorScheme={job.company === 'Justia' ? 'blue' : 'red'}
+                    borderRadius='md'
+                    px={2}
+                  >
+                    {job.company}
+                  </Code>
+                </Link>
+              </Flex>
               <Spacer />
               <Text color='gray.600'>{job.date}</Text>
             </Flex>
 
-            <Text color='gray.700'>{job.description}</Text>
-          </Box>
+            <Text color='gray.700' px={[2, 2, 0]}>
+              {job.description}
+            </Text>
+          </Stack>
         )
       })}
-    </Box>
+    </Stack>
   )
 }
 
