e9b155e06ca877ca183ad86b1a62ca71fc679e6c
jorgecontrerasostos <jorge.contreras@justia.com>
2024-02-07 15:28:15 -0600

    add speed insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,8 @@ import AnimatedSection from '@/components/AnimatedSection'
 import Layout from '@/components/layout/Layout'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+
 import '../styles/globals.css'
 import theme from '../theme/theme'
 
@@ -12,6 +14,7 @@ function MyApp({ Component, pageProps }) {
         <Layout>
           <Component {...pageProps} />
           <Analytics />
+          <SpeedInsights />
         </Layout>
       </AnimatedSection>
     </ChakraProvider>
