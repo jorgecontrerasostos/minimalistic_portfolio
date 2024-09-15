e65d55f54a4369832fa2f3c0557e5724556253dd
jorgecontrerasostos <jorge.contreras@justia.com>
2024-09-15 17:47:44 -0600

    updates

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,76 +9,76 @@ import {
   Link,
   Stack,
   Text,
-  useColorMode
-} from '@chakra-ui/react'
-import { motion } from 'framer-motion'
-import { BsArrowUpRightSquare, BsGithub } from 'react-icons/bs'
-import { projects } from '../data/projects'
+  useColorMode,
+} from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { BsArrowUpRightSquare, BsGithub } from "react-icons/bs";
+import { projects } from "../data/projects";
 
 const Projects = () => {
-  const { colorMode } = useColorMode()
-  const textColor = colorMode === 'light' ? 'gray.800' : 'gray.300'
+  const { colorMode } = useColorMode();
+  const textColor = colorMode === "light" ? "gray.800" : "gray.300";
 
   return (
     <>
       <Box my={8}>
         <Heading
-          fontSize='1.5em'
-          as='h2'
+          fontSize="1.5em"
+          as="h2"
           mb={-4}
-          textAlign={['center', 'center', 'center', 'left', 'left', 'left']}
-          letterSpacing='tight'
+          textAlign={["center", "center", "center", "left", "left", "left"]}
+          letterSpacing="tight"
         >
           Projects
         </Heading>
         {projects.map((project) => {
           return (
             <div key={project.id}>
-              <Stack direction={{ base: 'column', md: 'row' }}>
+              <Stack direction={{ base: "column", md: "row" }}>
                 <Flex flex={1} mt={10}>
-                  <Link href={project.link} target='_blank'>
+                  <Link href={project.link} target="_blank">
                     <motion.div whileHover={{ scale: 1.02 }}>
                       <Image
-                        alt='NASA Photo of the Day Project'
-                        objectFit={'cover'}
+                        alt="NASA Photo of the Day Project"
+                        objectFit={"cover"}
                         src={project.image}
-                        borderRadius='lg'
-                        shadow='lg'
+                        borderRadius="lg"
+                        shadow="lg"
                       />
                     </motion.div>
                   </Link>
                 </Flex>
-                <Flex p={8} flex={1} align={'center'} justify={'center'}>
-                  <Stack spacing={6} w={'full'} maxW={'lg'}>
+                <Flex p={8} flex={1} align={"center"} justify={"center"}>
+                  <Stack spacing={6} w={"full"} maxW={"lg"}>
                     <Heading
-                      fontSize='1.3em'
+                      fontSize="1.3em"
                       textAlign={[
-                        'center',
-                        'center',
-                        'left',
-                        'left',
-                        'left',
-                        'left'
+                        "center",
+                        "center",
+                        "left",
+                        "left",
+                        "left",
+                        "left",
                       ]}
-                      letterSpacing='tight'
+                      letterSpacing="tight"
                     >
                       <Text>{project.name}</Text>
                     </Heading>
 
-                    <Text fontSize='16px' color={textColor}>
+                    <Text fontSize="16px" color={textColor}>
                       {project.description}
                     </Text>
                     <Stack
-                      direction={{ base: 'row', md: 'row' }}
+                      direction={{ base: "row", md: "row" }}
                       spacing={4}
-                      justifyContent={{ base: 'center', md: 'center' }}
+                      justifyContent={{ base: "center", md: "center" }}
                     >
-                      <Link href={project.github} target='_blank'>
+                      <Link href={project.github} target="_blank">
                         <motion.div whileHover={{ scale: 1.08 }}>
                           <Icon as={BsGithub} boxSize={7} />
                         </motion.div>
                       </Link>
-                      <Link href={project.link} target='_blank'>
+                      <Link href={project.link} target="_blank">
                         <motion.div whileHover={{ scale: 1.08 }}>
                           <Icon as={BsArrowUpRightSquare} boxSize={7} />
                         </motion.div>
@@ -88,19 +88,14 @@ const Projects = () => {
                 </Flex>
               </Stack>
             </div>
-          )
+          );
         })}
-        <Box textAlign='center' py={6}>
-          <Text as='p' color={textColor}>
-            More projects coming soon...
-          </Text>
-        </Box>
       </Box>
       <Center>
-        <Divider borderColor='gray.300' w='10%' />
+        <Divider borderColor="gray.300" w="10%" />
       </Center>
     </>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
--- src/data/toolboxData.js
+++ src/data/toolboxData.js
@@ -1,52 +1,52 @@
 export const toolboxData = [
   {
     id: 0,
-    name: 'javascript',
-    bg: '#f7df1e'
-  },
-  {
-    id: 1,
-    name: 'python',
-    bg: '#2b5b84'
+    name: "python",
+    bg: "#2b5b84",
   },
   {
     id: 2,
-    name: 'sql',
-    bg: '#e97b00'
+    name: "sql",
+    bg: "#e97b00",
   },
   {
     id: 3,
-    name: 'chakra-ui',
-    bg: '#7fdbca'
+    name: "tableau",
+    bg: "#7fdbca",
+  },
+  {
+    id: 1,
+    name: "javascript",
+    bg: "#f7df1e",
   },
   {
     id: 4,
-    name: 'next-js',
-    bg: 'black'
+    name: "next-js",
+    bg: "black",
   },
   {
     id: 5,
-    name: 'assertive communication',
-    bg: '#0987a0'
+    name: "assertive communication",
+    bg: "#0987a0",
   },
   {
     id: 6,
-    name: 'analytical thinking',
-    bg: '#553c9a'
+    name: "analytical thinking",
+    bg: "#553c9a",
   },
   {
     id: 7,
-    name: 'time management',
-    bg: '#9b2c2c'
+    name: "time management",
+    bg: "#9b2c2c",
   },
   {
     id: 8,
-    name: 'collaboration',
-    bg: '#9c4221'
+    name: "collaboration",
+    bg: "#9c4221",
   },
   {
     id: 9,
-    name: 'adaptability',
-    bg: '#97266d'
-  }
-]
+    name: "adaptability",
+    bg: "#97266d",
+  },
+];
