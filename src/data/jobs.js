6cb9c7dbc2d8cfe53e70807f05860e7d9a3ccc4c
jorgecontrerasostos <jorge.contreras@justia.com>
2025-01-13 22:35:41 -0600

    update footer and more

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -1,25 +1,24 @@
-import CostcoIcon from "../components/CostcoIcon";
-import JustiaIcon from "../components/JustiaIcon";
+import JustiaIcon from '../components/JustiaIcon'
 
 export const jobs = [
   {
     id: crypto.randomUUID(),
 
-    title: "Web Services Associate",
-    company: "Justia",
+    title: 'Web Services Associate',
+    company: 'Justia',
     icon: JustiaIcon,
-    description: `I was offered a full time position when my internship ended. I'm continuously working on an automation tool to extract the content from old websites and migrate it to newer version as fast as possible preserving the integrity of the content. This tool uses Python as its main language`,
-    date: "June 2021 - Present",
-    link: "https://www.justia.com/",
+    description: `I was offered a full time position when my internship ended. I'm continuously working on an automation tool to extract the content from old websites and migrate it to newer version as fast as possible preserving the integrity of the content. This tool uses Python as its main language.`,
+    date: 'June 2021 - Present',
+    link: 'https://www.justia.com/'
   },
   {
     id: crypto.randomUUID(),
 
-    title: "Web Services Intern",
-    company: "Justia",
+    title: 'Web Services Intern',
+    company: 'Justia',
     icon: JustiaIcon,
     description: `This was the first IT related job I ever got. During my internship I worked on different tasks such as tweaking websites, adding content on our CMS, Quality Assurance and migrating content from one website to another.`,
-    date: "January 2021 - June 2021",
-    link: "https://www.justia.com/",
-  },
-];
+    date: 'January 2021 - June 2021',
+    link: 'https://www.justia.com/'
+  }
+]
