34d375c08affcfa5d789491836c1cc34cc496dcf
jorgecontrerasostos <jorge.contreras@justia.com>
2025-01-28 19:39:39 -0600

    chore: update icons

--- src/components/SwitchColorMode.jsx
+++ src/components/SwitchColorMode.jsx
@@ -1,6 +1,6 @@
-import { MoonIcon, SunIcon } from '@chakra-ui/icons'
-import { IconButton, useColorMode } from '@chakra-ui/react'
+import { Icon, IconButton, useColorMode } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
+import { LuMoon, LuSun } from 'react-icons/lu'
 
 const SwitchColorMode = () => {
   const { colorMode, toggleColorMode } = useColorMode()
@@ -11,19 +11,17 @@ const SwitchColorMode = () => {
         aria-label='Toggle color mode'
         icon={
           colorMode === 'dark' ? (
-            <SunIcon color='yellow.400' boxSize={6} />
+            <Icon as={LuSun} boxSize={6} color='white' />
           ) : (
-            <MoonIcon color='gray.900' boxSize={6} />
+            <Icon as={LuMoon} boxSize={6} color='black' />
           )
         }
         onClick={toggleColorMode}
         colorScheme='customSwitchColors'
         size='lg'
         isRound
-        bg='transparent' // Transparent background
+        bg='transparent'
         _hover={{ bg: 'transparent' }}
-        _active={{ bg: 'transparent' }}
-        _focus={{ bg: 'transparent' }}
       />
     </motion.div>
   )
