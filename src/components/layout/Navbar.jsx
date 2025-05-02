e3df93afd15be7e76d2e521727291b3df1f7f7ee
jorgecontrerasostos <jorge.contreras@justia.com>
2025-05-02 11:18:08 -0600

    navbar updates

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -12,10 +12,16 @@ const Navbar = () => {
         as='nav'
         w='100%'
         alignItems='center'
-        justifyContent='space-between'
+        justifyContent={[
+          'space-between',
+          'space-between',
+          'space-between',
+          'flex-start'
+        ]}
         py={4}
         px={[8, 8, 0, 0, 0, 0]}
       >
+        {/* Logo */}
         <Box>
           <Link href='/'>
             <motion.div whileHover={{ scale: 1.08 }}>
@@ -24,7 +30,28 @@ const Navbar = () => {
           </Link>
         </Box>
 
-        <HStack spacing={8} ml={[0, 0, 0, 'auto']}>
+        {/* Mobile Blog Link (centered) */}
+        <Box
+          display={['block', 'block', 'block', 'none']}
+          position='absolute'
+          left='50%'
+          transform='translateX(-50%)'
+        >
+          <ChakraLink
+            _hover={{ textDecoration: 'none' }}
+            href='/blog'
+            as={Link}
+          >
+            blog
+          </ChakraLink>
+        </Box>
+
+        {/* Desktop Nav Items */}
+        <HStack
+          spacing={8}
+          ml={[0, 0, 0, 'auto']}
+          display={['none', 'none', 'none', 'flex']}
+        >
           <ChakraLink
             _hover={{ textDecoration: 'none' }}
             href='/blog'
@@ -37,6 +64,11 @@ const Navbar = () => {
             <SwitchColorMode />
           </Box>
         </HStack>
+
+        {/* Mobile Color Switch */}
+        <Box display={['block', 'block', 'block', 'none']}>
+          <SwitchColorMode />
+        </Box>
       </Flex>
     </Box>
   )
