96e598e2e3c2b3791ce8b366c19db51e4fca25b7
Jorge Contreras <jorgecontrerasostos@gmail.com>
2026-01-13 10:06:34 -0600

    update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,59 +1,68 @@
 export const projects = [
   {
     id: crypto.randomUUID(),
-    name: 'ETL Pipeline Using Airflow, dbt and AWS',
+    name: "Dilo con Data",
     description:
-      'This project demonstrates the development of a comprehensive ETL data pipeline for e-commerce analytics, leveraging Apache Airflow for orchestration, dbt for data transformation, and Amazon Web Services (AWS) for cloud infrastructure.',
-    image: '/etl-pipeline.png',
+      "A data-based platform where I post articles about climate change, global warming, and sustainability in non-technical language so everyone can understand the complexity of these topics. All posts are in Spanish since the content is intended for Latin American audiences.",
+    image: "/dilo-con-data.png",
+    github: "https://github.com/jorgecontrerasostos/",
+    link: "https://www.dilocondata.com/",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "ETL Pipeline Using Airflow, dbt and AWS",
+    description:
+      "This project demonstrates the development of a comprehensive ETL data pipeline for e-commerce analytics, leveraging Apache Airflow for orchestration, dbt for data transformation, and Amazon Web Services (AWS) for cloud infrastructure.",
+    image: "/etl-pipeline.png",
     github:
-      'https://github.com/jorgecontrerasostos/etl-pipeline-using-airflow-dbt-and-aws',
-    link: 'https://app.hex.tech/01971dd9-d377-7005-80b2-4124e264052b/app/01977166-bc21-700c-8990-f501a4b1c810/latest'
+      "https://github.com/jorgecontrerasostos/etl-pipeline-using-airflow-dbt-and-aws",
+    link: "https://app.hex.tech/01971dd9-d377-7005-80b2-4124e264052b/app/01977166-bc21-700c-8990-f501a4b1c810/latest",
   },
   {
     id: crypto.randomUUID(),
-    name: 'Flight Delay Prediction Pipeline Using PySpark',
+    name: "Flight Delay Prediction Pipeline Using PySpark",
     description:
-      'Utilized PySpark and SQL queries for large-scale data cleaning, feature engineering, and transformation, including handling missing values, indexing categorical variables, and assembling feature vectors for machine learning.',
-    image: '/flight_delay.jpeg',
+      "Utilized PySpark and SQL queries for large-scale data cleaning, feature engineering, and transformation, including handling missing values, indexing categorical variables, and assembling feature vectors for machine learning.",
+    image: "/flight_delay.jpeg",
     github:
-      'https://github.com/jorgecontrerasostos/flight-delay-prediction-pipeline/blob/main/README.md',
-    link: 'https://github.com/jorgecontrerasostos/flight-delay-prediction-pipeline/blob/main/main.ipynb'
+      "https://github.com/jorgecontrerasostos/flight-delay-prediction-pipeline/blob/main/README.md",
+    link: "https://github.com/jorgecontrerasostos/flight-delay-prediction-pipeline/blob/main/main.ipynb",
   },
   {
     id: crypto.randomUUID(),
-    name: 'Machine Learning Models Comparison to Detect Heart Diseases',
+    name: "Machine Learning Models Comparison to Detect Heart Diseases",
     description:
-      'Compared different ML models to analyze which one was the most suitable for the data representing better metrics',
-    image: '/heart_disease.jpeg',
+      "Compared different ML models to analyze which one was the most suitable for the data representing better metrics",
+    image: "/heart_disease.jpeg",
     github:
-      'https://github.com/jorgecontrerasostos/machine-learning-models-comparison-to-detect-heart-diseases/blob/main/main.ipynb',
-    link: 'https://github.com/jorgecontrerasostos/machine-learning-models-comparison-to-detect-heart-diseases/blob/main/main.ipynb'
+      "https://github.com/jorgecontrerasostos/machine-learning-models-comparison-to-detect-heart-diseases/blob/main/main.ipynb",
+    link: "https://github.com/jorgecontrerasostos/machine-learning-models-comparison-to-detect-heart-diseases/blob/main/main.ipynb",
   },
   {
     id: crypto.randomUUID(),
-    name: 'Unemployment Rate Data Project (Spanish)',
+    name: "Unemployment Rate Data Project (Spanish)",
     description:
-      'Exploratory Data Analysis with useful insights about the unemployment rate in various countries using Pandas and Seaborn.',
-    image: '/unemployment_rate_analysis.png',
-    github: 'https://github.com/jorgecontrerasostos/unemployment-rate-analysis',
-    link: 'https://github.com/jorgecontrerasostos/unemployment-rate-analysis/blob/main/unemployment_analysis.ipynb'
+      "Exploratory Data Analysis with useful insights about the unemployment rate in various countries using Pandas and Seaborn.",
+    image: "/unemployment_rate_analysis.png",
+    github: "https://github.com/jorgecontrerasostos/unemployment-rate-analysis",
+    link: "https://github.com/jorgecontrerasostos/unemployment-rate-analysis/blob/main/unemployment_analysis.ipynb",
   },
   {
     id: crypto.randomUUID(),
-    name: 'MLB Pitches Data Visualizations Using Polars and Seaborn',
+    name: "MLB Pitches Data Visualizations Using Polars and Seaborn",
     description:
-      'Analyzed CSV files related to 2018 Major League Baseball pitches statistics using Polars and Seaborn.',
-    image: '/mlb_pitches_visualization.png',
-    github: 'https://github.com/jorgecontrerasostos/mlb-data-visualization',
-    link: 'https://github.com/jorgecontrerasostos/mlb-data-visualization/blob/master/mlb-data-visualization.ipynb'
+      "Analyzed CSV files related to 2018 Major League Baseball pitches statistics using Polars and Seaborn.",
+    image: "/mlb_pitches_visualization.png",
+    github: "https://github.com/jorgecontrerasostos/mlb-data-visualization",
+    link: "https://github.com/jorgecontrerasostos/mlb-data-visualization/blob/master/mlb-data-visualization.ipynb",
   },
   {
     id: crypto.randomUUID(),
-    name: 'NASA Photo of the Day',
+    name: "NASA Photo of the Day",
     description:
-      'NASA Photo of the Day is a web app that, using the NASA API, displays the NASA photo of the day along with its explanation.',
-    image: '/nasa_photo_of_the_day.png',
-    github: 'https://github.com/jorgecontrerasostos/nasa-daily-photo',
-    link: 'https://nasa-daily-photo-eosin.vercel.app/'
-  }
-]
+      "NASA Photo of the Day is a web app that, using the NASA API, displays the NASA photo of the day along with its explanation.",
+    image: "/nasa_photo_of_the_day.png",
+    github: "https://github.com/jorgecontrerasostos/nasa-daily-photo",
+    link: "https://nasa-daily-photo-eosin.vercel.app/",
+  },
+];
